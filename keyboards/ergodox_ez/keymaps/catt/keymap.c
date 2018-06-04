ec6cb667711a2b0886766f876675f079442e3ca0
catt-io <catt-io@users.noreply.github.com>
2018-06-04 19:14:58 +1200

    ...

--- keyboards/ergodox_ez/keymaps/catt/keymap.c
+++ keyboards/ergodox_ez/keymaps/catt/keymap.c
@@ -12,15 +12,20 @@
 #define BLAN 6 // media keys
 
 enum custom_keycodes {
-  PLACEHOLDER = SAFE_RANGE, // can always be here
+  PLACEHOLDER = SAFE_RANGE, // can aways be here
   CK_RARW,
   CK_RFRW,
 };
 
+#define SS_SPOT LGUI(KC_SPC)
+#define SS_MTOG LGUI(KC_TAB)
+#define SS_WTOG LALT(KC_TAB)
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* 
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |  F1  |  F2  |  F3  |  F4  |  F5  | F11  |           | F12  |  F6  |  F7  |  F8  |  F9  |  F0  |        |
+ * | Toggle |  F1  |  F2  |  F3  |  F4  |  F5  | F11  |           | F12  |  F6  |  F7  |  F8  |  F9  |  F0  |  Spot  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   F  |   P  |   G  |  L4  |           |  Up  |   J  |   L  |   U  |   Y  |   '  |  LGui  |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -31,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   |LCtrl | LGui | LGui | LAlt |LCtrl |                                       |RCtrl | RAlt | RGui | RGui | RCtrl |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |Hyper |      |       | LGui |Hyper |
+ *                                        |Hyper |      |       |      |Hyper |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | L6   |       | L1   |        |      |
  *                                 |Space | Esc  |------|       |------| Enter  |BkSpac|
@@ -39,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 `--------------------'       `----------------------'
  */
 [BASE] = LAYOUT_ergodox(
-        KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11,
+        SS_MTOG, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11,
         KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    MO(NAVI),
         MO(SYMB),KC_A,    KC_R,    KC_S,    KC_T,    KC_D,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    MO(NUMP),
@@ -48,18 +53,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                               TG(GAME),
                                             KC_SPC,  KC_ESC,  KC_MEH,
 
-        KC_F12,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_TRNS,
+        KC_F12,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  SS_SPOT,
         KC_UP,   KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT, KC_LGUI,
                  KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    MO(SYMB),
         KC_DOWN, KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SCLN, KC_RSFT,
                           KC_RCTL, KC_RALT, KC_RGUI, KC_RGUI, KC_RCTL,
-        KC_RGUI, KC_HYPR,
+        KC_TRNS, KC_HYPR,
         TG(WIND),
         KC_MEH,  KC_ENT,  KC_BSPC
 ),
 /* 
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |WToggle |      |      |      |      |      |      |           |      |      |      |      |      |      |  Win   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |      |      |      |      |      |      |           |      |      |      |      |      |      | LCtrl  |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -78,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 `--------------------'       `----------------------'
  */
 [WIND] = LAYOUT_ergodox(
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        SS_WTOG, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -87,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                               KC_TRNS,
                                             KC_TRNS, KC_TRNS, KC_TRNS,
 
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LGUI,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LCTL,
                  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
