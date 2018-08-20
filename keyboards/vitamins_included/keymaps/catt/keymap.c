1f3ee43cdc531d2df239b12f78f9e0af27cda85e
catt-io <catt-io@users.noreply.github.com>
2018-08-20 22:24:58 +1200

    ...

--- keyboards/vitamins_included/keymaps/catt/keymap.c
+++ keyboards/vitamins_included/keymaps/catt/keymap.c
@@ -40,17 +40,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [SYMB] = LAYOUT_ortho_4x12( \
-  _______,         KC_DLR,  KC_HASH, KC_AT,   KC_GRAVE,KC_TILD, _______, KC_PIPE, KC_PERC, KC_CIRC, KC_DQUO, _______, \
+  _______,         KC_DLR,  KC_HASH, KC_AT,   KC_GRAVE,KC_TILD, SS_SPOT, KC_PIPE, KC_PERC, KC_CIRC, KC_DQUO, _______, \
   _______,         KC_LABK, KC_LBRC, KC_LCBR, KC_LPRN, KC_QUES, KC_EXLM, KC_PLUS, KC_ASTR, KC_EQL,  KC_UNDS, _______, \
   _______,         KC_RABK, KC_RBRC, KC_RCBR, KC_RPRN, KC_TAB,  KC_AMPR, KC_MINS, KC_SLSH, KC_BSLS, KC_COLN, _______, \
-  RESET,           _______, _______, _______, KC_UP,   KC_ESC,  KC_ENT,  KC_DOWN, _______, _______, _______, RESET \
+  _______,         _______, _______, KC_LEFT, KC_UP,   KC_ESC,  KC_ENT,  KC_DOWN, KC_RIGHT,_______, _______, _______ \
 ),
 
 [NUMP] = LAYOUT_ortho_4x12( \
-  _______,         _______, _______, KC_UP,   _______, _______, _______, KC_1,    KC_2,    KC_3,    KC_HASH, _______, \
-  _______,         _______, KC_LEFT, KC_DOWN, KC_RIGHT,_______, KC_DOT,  KC_4,    KC_5,    KC_6,    KC_0,    _______, \
-  _______,         _______, _______, _______, _______, _______, _______, KC_7,    KC_8,    KC_9,    KC_DLR,  _______, \
-  _______,         _______, _______, _______, _______, _______, KC_DEL,  _______, _______, _______, _______, _______ \
+  _______,         TG(WIND),KC_F1,   KC_F2,   KC_F3,   _______, _______, KC_1,    KC_2,    KC_3,    _______, _______, \
+  _______,         _______, KC_F4,   KC_F5,   KC_F6,   KC_LEFT, KC_UP,   KC_4,    KC_5,    KC_6,    KC_0,    _______, \
+  _______,         _______, KC_F7,   KC_F8,   KC_F9,   KC_RIGHT,KC_DOWN, KC_7,    KC_8,    KC_9,    KC_DOT,  _______, \
+  RESET,           _______, KC_F10,  KC_F11,  KC_F12,  _______, KC_DEL,  _______, _______, _______, _______, RESET \
 ),
 
 [BLAN] = LAYOUT_ortho_4x12( \
